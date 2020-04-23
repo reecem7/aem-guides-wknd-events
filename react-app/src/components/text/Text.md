dfbf9797109e9da1ee0b51235b23f73df8e441a7
Reece Maganzini <reece.maganzini@credera.com>
2020-04-23 01:00:58 -0500

    Changes from first tutorial completion with chapter 3 causing issues

---
+++ react-app/src/components/text/Text.md
@@ -0,0 +1,12 @@
+Plain Text:
+ 
+  ```js
+  <Text text="hello world"/>
+  ```
+
+Rich Text:
+ 
+  ```js
+  <Text text="<h1>hello world</h1>"
+    richText="true"/>
+  ```
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,32 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ModelManager, Constants } from '@adobe/cq-spa-page-model-manager';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./components/MappedComponents";
+import './index.scss';
+import {BrowserRouter} from 'react-router-dom';
+import ScrollToTop from './utils/RouteHelper';
+import { Redirect, Route } from 'react-router';
+ 
+function render(model) {
+    ReactDOM.render((
+        <BrowserRouter>
+            <ScrollToTop>
+            <Route path="/content/wknd-events/react.html" render={() => (
+                <Redirect to="/content/wknd-events/react/home.html"/>
+            )}/>
+                <App 
+                cqChildren={ model[Constants.CHILDREN_PROP] } 
+                cqItems={ model[Constants.ITEMS_PROP] } 
+                cqItemsOrder={ model[Constants.ITEMS_ORDER_PROP] }
+                cqPath={ ModelManager.rootPath } 
+                locationPathname={ window.location.pathname }
+                />
+            </ScrollToTop>
+        </BrowserRouter>), 
+        document.getElementById('root'));
+}
+
+ModelManager.initialize({ path: process.env.REACT_APP_PAGE_MODEL_PATH }).then(render);
